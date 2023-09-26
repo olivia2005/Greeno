635175474eee9d59a9dc6da1a68551a30f9ca394
olivia2004 <andreea_olivia_ionica@yahoo.com>
2023-09-26 17:28:37 +0300

    yay

---
+++ src/test/java/utils/BrowserFactory.java
@@ -0,0 +1,42 @@
+package utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BrowserFactory {
+
+    private static WebDriver driver;
+
+    private BrowserFactory() {
+        // Private constructor to prevent instantiation
+    }
+
+    public static WebDriver getDriver() {
+        if (driver == null) {
+            String browser = ConfigReader.getPropertyValue("browser");
+            switch (browser.toLowerCase()) {
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    break;
+                // You can add more browsers here if needed
+                default:
+                    throw new RuntimeException("Unsupported browser: " + browser);
+            }
+        }
+        return driver;
+    }
+
+    public static void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null; // Set to null so a new instance can be created on the next call
+        }
+    }
+}
